b2d89071d7ba1b982becf566be63189c9c91c9bf
R-Hesham <rehabhesham.work@gmail.com>
2023-11-18 12:36:44 +0200

    add screenshots

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 import os
-from datetime import datetime
 
 
 # Configure application
